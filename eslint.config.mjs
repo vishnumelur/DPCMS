4dad2d4882aeab139ed6416eb64b6ad6508b2c0d
Vishnu Manoj <vishnu.melur@gmail.com>
2026-05-25 11:59:31 +0530

    chore(p0): eslint flat config + prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
+
+const config = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+];
+
+export default config;
